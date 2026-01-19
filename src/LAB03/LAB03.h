99a331cbbc7f0256db68026b5f61b2bf0c437db6
Harry Clark <139020900+astonHC@users.noreply.github.com>
2026-01-19 15:08:04 +0000

    experimenting with directive clauses for includes

---
+++ src/LAB03/LAB03.h
@@ -0,0 +1,25 @@
+// COPYRIGHT (C) HARRY CLARK 2026
+// CS2_OS LAB AND TUTORIAL CONTENTS
+
+// THIS FILE PERTAINS TOWARDS THE MAIN FUNCTIONALITY OF LAB 03
+// FOCUSSING ON PROVIDING SOME SORT OF MODULARISATION OF FUNCTIONALITY
+
+#ifndef CS2_OS_LAB_03
+#define CS2_OS_LAB_03
+
+// SYSTEM INCLUDES  
+
+#include <string.h>
+
+#if defined(EXERCISE_3_1)
+    #include <stdio.h>
+
+#elif defined(EXERCISE_3_2)
+    #include <unistd.h>
+
+#elif defined(EXERCISE_3_3)
+    #include <unistd.h>
+    #include <sys/syscall.h>
+
+#endif
+#endif
